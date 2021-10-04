e946a84d659df15eaaf20e128b1e2835835fe7f6
hiepnguyen223 <hiepnguyenn223@gmail.com>
2021-10-04 22:21:13 +0700

    them phan chay am thanh

--- src/DictionaryApp/Main.java
+++ src/DictionaryApp/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
         primaryStage.setTitle("Dictionary");
         Scene scene = new Scene(root);
